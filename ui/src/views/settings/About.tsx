c965061e86ddb6913689af8a1a4a4fc53eb4d72d
zhengyi59 <admin@mczhengyi.top>
2024-08-25 15:35:00 +0800

    ✨Add update all server to ui

--- ui/src/views/settings/About.tsx
+++ ui/src/views/settings/About.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import { Button, Flex } from 'antd';
+import { Button, Flex, message } from 'antd';
 import Logo from '../../assets/docker-mark-blue.svg';
 import { ArrowUpOutlined } from '@ant-design/icons';
+import apiRequest from '../../api/api-request.ts';
+import { NodeData } from 'common/dist/types/daemon';
 
 const About: React.FC = () => {
+  const updateAllDaemon = () => {
+    apiRequest.get('overview/servers').then((res) => {
+      const activeServer = res.data.servers.filter((s: NodeData) => s.nodeInfo.nodeStatus === 'connected');
+      activeServer.map((s: NodeData) =>
+        apiRequest.post(`nodes/${s.nodeName}/update`, {}).then((_) => {
+          message.success(`${s.nodeName} 更新成功，请等待daemon重启`);
+        }),
+      );
+    });
+  };
+
   return (
     <Flex vertical gap={20} align={'center'} justify={'center'}>
       <img src={Logo} alt="logo" style={{ width: 100 }} />
@@ -13,6 +26,9 @@ const About: React.FC = () => {
       </div>
       <Button type={'primary'} icon={<ArrowUpOutlined />} disabled>
         检查更新
+      </Button>
+      <Button type={'primary'} icon={<ArrowUpOutlined />} onClick={updateAllDaemon}>
+        强制更新所有daemon
       </Button>
       <div>
         Developed By zhengyi59 <a href="https://github.com/justice2001/docker-mng">GitHub</a>
